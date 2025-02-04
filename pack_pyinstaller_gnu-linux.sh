a9220d5dcf3fb356dd18e6f694d388df955d4e17
Fersilent <ap3orexhx@relay.firefox.com>
2025-02-04 15:34:52 -0500

    Add pack_pyinstaller_gnu-linux.sh

---
+++ pack_pyinstaller_gnu-linux.sh
@@ -0,0 +1,16 @@
+# Mount virtual environment and install packages
+pyenv shell 3.8.10
+python -V
+python -m venv venv
+source venv/bin/activate
+pip install -r requirements.txt
+
+# Application Packaging
+pyinstaller "ProgrammingDirectories.spec"
+
+# Copy application directories and files
+cp -r src/controller/ dist/ProgrammingDirectories/
+cp -r src/model/ dist/ProgrammingDirectories/
+cp -r src/view/ dist/ProgrammingDirectories/
+cp -r src/static/ dist/ProgrammingDirectories/
+cp src/config.py dist/ProgrammingDirectories/
