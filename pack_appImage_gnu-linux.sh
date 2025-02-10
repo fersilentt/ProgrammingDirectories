df788b3adeee369f095488d0691fd5aa334284ea
Fersilent <ap3orexhx@relay.firefox.com>
2025-02-10 15:39:37 -0500

    Update pack_pyinstaller_gnu-linux.sh

--- pack_appImage_gnu-linux.sh
+++
@@ -1,9 +0,0 @@
-mv dist/ProgrammingDirectories/ dist/usr
-
-mkdir dist/ProgrammingDirectories.AppDir
-
-mv dist/usr dist/ProgrammingDirectories.AppDir
-
-mv dist/ProgrammingDirectories.AppDir/usr/ProgrammingDirectories.desktop dist/ProgrammingDirectories.AppDir
-
-mv dist/ProgrammingDirectories.AppDir/usr/ProgrammingDirectories.png dist/ProgrammingDirectories.AppDir
--- pack_pyinstaller_gnu-linux.sh
+++ pack_pyinstaller_gnu-linux.sh
@@ -12,6 +12,7 @@ pyinstaller "ProgrammingDirectories.spec"
 # Copy application directories and files
 cp resources/img/ProgrammingDirectories.png dist/ProgrammingDirectories
 
+# Create the shortcut
 path=dist/ProgrammingDirectories/ProgrammingDirectories.desktop
 
 echo "[Desktop Entry]" >> $path
@@ -19,7 +20,7 @@ echo "Type=Application" >> $path
 echo "Name=ProgrammingDirectories" >> $path
 echo "Comment=Manage projects and programming tutorials" >> $path
 echo "Exec=ProgrammingDirectories" >> $path
-echo "Icon=ProgrammingDirectories.png" >> $path
+echo "Icon=ProgrammingDirectories" >> $path
 echo "Categories=Network;" >> $path
 
 chmod +x $path
